6ef4c3ffc5f22c11f88c94397f9689f0559d5414
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-07-17 02:42:44 +0500

    wallpaper app complete

--- wallpaper_hub/lib/controller/api_controller.dart
+++ wallpaper_hub/lib/controller/api_controller.dart
@@ -1,16 +1,41 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:wallpaper_hub/data/api_key.dart';
-import 'dart:convert' as convert;
 
-Future getTrendingWallpapers() async {
-  var response = await http
-      .get(Uri.parse("https://api.pexels.com/v1/curated?per_page=1"), headers: {
-    "Authorization": APIKEY,
-  });
+import 'package:wallpaper_hub/models/wallpaper_model.dart';
 
-  // print(response.body.toString());
-  Map<String, dynamic> jsonData = convert.jsonDecode(response.body);
-  jsonData['photos'].forEach((element) {
-    print(element);
-  });
+getTrendingWallpapers() async {
+  List<Photos> wallpapers = [];
+  var response = await http.get(
+      Uri.parse("https://api.pexels.com/v1/curated?per_page=40"),
+      headers: {
+        "Authorization": APIKEY,
+      });
+
+  if (response.statusCode == 200) {
+    var json = jsonDecode(response.body);
+    var data = WallpaperModel.fromJson(json);
+    wallpapers = data.photos!;
+    print(data.photos![0].src!.portrait);
+    return wallpapers;
+  }
+}
+
+getCategoryWallpapers(String category) async {
+  List<Photos> wallpapers = [];
+  var response = await http.get(
+      Uri.parse(
+          "https://api.pexels.com/v1/search?query=$category&per_page=30&page=1"),
+      headers: {
+        "Authorization": APIKEY,
+      });
+
+  if (response.statusCode == 200) {
+    var json = jsonDecode(response.body);
+    var data = WallpaperModel.fromJson(json);
+    wallpapers = data.photos!;
+    print(data.photos![0].src!.portrait);
+    return wallpapers;
+  }
 }
--- wallpaper_hub/lib/views/home_screen.dart
+++ wallpaper_hub/lib/views/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:wallpaper_hub/controller/api_controller.dart';
+import 'package:wallpaper_hub/views/categories_screen.dart';
 
 import '../models/categories_model.dart';
 import '../widgets/brand_name.dart';
 import 'package:wallpaper_hub/data/data.dart';
+
+import '../widgets/category_tile.dart';
+import '../widgets/grid_wallpapers.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,14 +16,20 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   List<CategoriesModel> category = [];
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
-    getTrendingWallpapers();
     category = categories;
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -30,81 +40,109 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const BrandName(),
         elevation: 0.0,
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 24),
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: const Color(0xfff5f8fd),
-            ),
-            child: const Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search...",
-                      border: InputBorder.none,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                color: const Color(0xfff5f8fd),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        hintText: "Search...",
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                ),
-                Icon(Icons.search),
-              ],
+                  InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) =>
+                                CategoriesScreen(name: _searchController.text),
+                          ),
+                        );
+                      },
+                      child: const Icon(Icons.search)),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            height: 80,
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: category.length,
-              itemBuilder: (context, index) {
-                return CategoryTile(category: category[index]);
-              },
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 80,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: category.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CategoriesScreen(
+                                name: category[index].cotegoryName),
+                          ));
+                    },
+                    child: CategoryTile(
+                      category: category[index],
+                    ),
+                  );
+                },
+              ),
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class CategoryTile extends StatelessWidget {
-  const CategoryTile({super.key, required this.category});
-
-  final CategoriesModel category;
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.network(
-            category.imageUrl,
-            width: 100,
-            height: 50,
-            fit: BoxFit.cover,
-          ),
+            FutureBuilder(
+                future: getTrendingWallpapers(),
+                builder: (ctx, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      width: double.infinity,
+                      child: GridWallpapers(data: snapshot.data),
+                      // child: GridView.builder(
+                      //     itemCount: snapshot.data.length,
+                      //     padding: const EdgeInsets.symmetric(horizontal: 16),
+                      //     physics: const ClampingScrollPhysics(),
+                      //     gridDelegate:
+                      //         const SliverGridDelegateWithFixedCrossAxisCount(
+                      //       crossAxisCount: 2,
+                      //       mainAxisSpacing: 6.0,
+                      //       crossAxisSpacing: 6.0,
+                      //       childAspectRatio: 0.6,
+                      //     ),
+                      //     itemBuilder: (ctx, index) {
+                      //       return ClipRRect(
+                      //         borderRadius: BorderRadius.circular(16),
+                      //         child: Image.network(
+                      //           snapshot.data[index].src.portrait,
+                      //           fit: BoxFit.cover,
+                      //         ),
+                      //       );
+                      //     }),
+                    );
+                  }
+                  return Container(
+                    color: Colors.red,
+                  );
+                }),
+          ],
         ),
-        Container(
-          width: 100,
-          height: 50,
-          margin: const EdgeInsets.only(right: 16),
-          alignment: Alignment.center,
-          child: Text(
-            category.cotegoryName,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
